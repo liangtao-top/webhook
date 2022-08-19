d3a1f19987b89ec98e56d3eb13402c84f6bf50a3
Iceㄨ涛 <liangtao.gz@foxmail.com>
2022-08-19 21:00:27 +0800

    增加后台模式

--- script/start.sh
+++ script/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#nohup ./dist/webhook-linux -sh "git --version" -file "/app/script/git/git-tag-develop.sh" -ticker 3600 -cron "/app/script/dev/backup-mysql.sh" &
 ./dist/webhook-linux -sh "git --version" -file "/app/script/git/git-tag-develop.sh" -ticker 3600 -cron "/app/script/dev/backup-mysql.sh"
