f0fa4b70834a934a990131a7b5635635ace803ce
Iceㄨ涛 <liangtao.gz@foxmail.com>
2022-09-20 12:03:31 +0800

    新增docker化的指令 up|stop|restart|status

--- script/start.sh
+++ script/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup ./dist/webhook-linux -file "xxx.sh" -cron "/xxx.sh" -ticker 3600 >/dev/null 2>&1 &
+nohup ./dist/webhook-linux -file "xxx.sh" -token "xxx" -cron "backup-xxx.sh:3600" >/dev/null 2>&1 &
