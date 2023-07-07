6666ff10cfee2d1414dc0543e03d2d0b8ab3ee0e
ABON-德意馨苑-8栋-1单元-401号-梁涛-PC <liangtao.gz@foxmail.com>
2023-07-07 13:40:40 +0800

    优化shell脚本

--- script/dev.sh
+++ script/dev.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$1" == "-d" ]; then
-  nohup ./dist/webhook-linux -p 9527 -context "/khzkYETe6gWv5eC1" -file "/app/script/git/git-tag-develop.sh" -token "u25839Vmx1Fu71mJUIkD1eaAOIeScxDx" -cron "/app/script/backup/backup-mysql.sh:3600,/app/script/backup/backup-www.sh:86400" >/dev/null 2>&1 &
+  nohup ./dist/webhook-linux -p 9527 -context "/khzkYETe6gWv5eC1" -file "/app/script/git/git-tag-develop.sh" -token "u25839Vmx1Fu71mJUIkD1eaAOIeScxDx" -cron "/app/script/backup/backup-mysql.sh:604800,/app/script/backup/backup-www.sh:86400" >/dev/null 2>&1 &
   nohup ./dist/webhook-linux -p 9528 -context "/khzkYETe6gWv5eC1" -file "/web/script/git-tag-develop.sh" -token "u25839Vmx1Fu71mJUIkD1eaAOIeScxDx" >/dev/null 2>&1 &
 else
-  ./dist/webhook-linux -p 9527 -context "/khzkYETe6gWv5eC1" -file "/app/script/git/git-tag-develop.sh" -token "u25839Vmx1Fu71mJUIkD1eaAOIeScxDx" -cron "/app/script/backup/backup-mysql.sh:3600,/app/script/backup/backup-www.sh:86400" &
+  ./dist/webhook-linux -p 9527 -context "/khzkYETe6gWv5eC1" -file "/app/script/git/git-tag-develop.sh" -token "u25839Vmx1Fu71mJUIkD1eaAOIeScxDx" -cron "/app/script/backup/backup-mysql.sh:604800,/app/script/backup/backup-www.sh:86400" &
   ./dist/webhook-linux -p 9528 -context "/khzkYETe6gWv5eC1" -file "/web/script/git-tag-develop.sh" -token "u25839Vmx1Fu71mJUIkD1eaAOIeScxDx"
 fi
